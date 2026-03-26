045ee73c9ba423a876098157ebf68e62e13d47d6
AksaMJML <akaks0186@gmail.com>
2026-03-26 21:52:11 +0530

    feat: add CRUD operations to ItemController for item management

--- src/main/java/edu/icet/controller/ItemController.java
+++ src/main/java/edu/icet/controller/ItemController.java
@@ -1,9 +1,11 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.ItemDTO;
 import edu.icet.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("item")
@@ -11,5 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
     private final ItemService itemService;
 
+    @PostMapping
+    public boolean addItem(@RequestBody ItemDTO itemDTO){
+        return itemService.addItem(itemDTO);
+    }
+
+    @PutMapping
+    public boolean upadteItem(@RequestBody ItemDTO itemDTO){
+        return itemService.updateItem(itemDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteById(@@PathVariable String id){
+        return itemService.deleteItem(id);
+    }
+
+    @GetMapping("/{id}")
+    public ItemDTO searchById(@PathVariable String id){
+        return itemService.serachItem(id);
+    }
+
+    @GetMapping
+    public List<ItemDTO> getAll(){
+        return itemService.getAll();
+    }
 
 }
